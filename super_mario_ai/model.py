6425b0c5d01b8997bb26ad091ea04f66f4a77bd7
thomasdtran <thian0703@gmail.com>
2021-11-17 00:25:17 +0100

    Created two seperated networks for actor and critic

--- super_mario_ai/model.py
+++ super_mario_ai/model.py
@@ -9,12 +9,17 @@ class ActorCritic(nn.Module):
 
         c, h, w = input_n
         # Model layers (includes initialized model variables):
-        self.conv1 = nn.Conv2d(c, 32, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        self.linear = nn.Linear(64 * 7 * 7, 512)
+        self.critic_conv1 = nn.Conv2d(c, 32, kernel_size=8, stride=4)
+        self.critic_conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.critic_conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.critic_dense = nn.Linear(64 * 7 * 7, 512)
+
+        self.actor_conv1 = nn.Conv2d(c, 32, kernel_size=8, stride=4)
+        self.actor_conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.actor_conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.actor_dense = nn.Linear(64 * 7 * 7, 512)
     
-        #Activation function to get the estimate of value function
+        #Activation function to get the estimate of value functions
         #84 is the size of the input frame?/state?
         self.critic_linear = nn.Linear(512, 1)
         #Activation function to get policy distribution
@@ -25,12 +30,18 @@ class ActorCritic(nn.Module):
         state = torch.tensor(state)
         state = state.unsqueeze(0)
 
-        state = F.relu((self.conv1(state)))
-        state = F.relu((self.conv2(state)))
-        state = F.relu((self.conv3(state)))
-        state = F.relu(self.linear(state.reshape(-1, 64 * 7 * 7)))
-    
+        value = F.relu((self.critic_conv1(state)))
+        value = F.relu((self.critic_conv2(state)))
+        value = F.relu((self.critic_conv3(state)))
+        value = F.relu(self.critic_dense(state.reshape(-1, 64 * 7 * 7)))
+
         value = self.critic_linear(state)
+
+        policy = F.relu((self.actor_conv1(state)))
+        policy = F.relu((self.actor_conv2(state)))
+        policy = F.relu((self.actor_conv3(state)))
+        policy = F.relu(self.actor_dense(state.reshape(-1, 64 * 7 * 7)))
+
         policy = F.softmax(self.actor_linear(state), dim=1)
 
         return value, policy
